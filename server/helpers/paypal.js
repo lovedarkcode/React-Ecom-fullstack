e0c29e290abd7b69016340a56381651f426adac1
Aditya <139672646+lovedarkcode@users.noreply.github.com>
2024-11-29 10:36:14 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,7 +2,7 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: Process.env.PAYPAL_CLIENT_ID,
+  client_id: process.env.PAYPAL_CLIENT_ID,
   client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
